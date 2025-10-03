f4f331645231c7257204d0c146253894412ca13a
Joseph 1864 <josephwilliamsm99@gmail.com>
2025-10-02 19:20:31 -0700

    feat: CreateTaskDialog

---
+++ app/src/main/java/com/example/applicationtemplate/screens/home/CreateTaskDialog.kt
@@ -0,0 +1,47 @@
+package com.example.applicationtemplate.screens.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CreateTaskDialog(
+    title: String,
+    description: String,
+    onTitleChanged: (String) -> Unit,
+    onDescriptionChanged: (String) -> Unit,
+    onDismiss: () -> Unit,
+    onSubmit: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text("Create Task") },
+        text = {
+            Column {
+                OutlinedTextField(
+                    value = title,
+                    onValueChange = onTitleChanged,
+                    label = { Text("Title") }
+                )
+                OutlinedTextField(
+                    value = description,
+                    onValueChange = onDescriptionChanged,
+                    label = { Text("Description") }
+                )
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onSubmit) {
+                Text("Create")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    )
+}
